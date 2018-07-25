70e9cf68ab3c31dc705717d13619d57a4885de11
matthew <mthwbrwn@gmail.com>
2018-07-25 11:23:55 -0700

    created book view object - re-arranged files

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,21 @@ var app = app || {};
 (function (module){
 
 //TODO create show only property "isProduction"
+module.isProduction= /^(?!localhost\127)/.test(window.location.hostname);
+
+// TODO creat "show only method to reveal containers"- helper functions
+module.showOnly =  (selector) => {
+    $('.container').hide();
+    $(selector).show();
+};
+
+//TODO create render only method  - needs to use module -helper function
+module.render = (templateId, data)  =>  {
+    if(!module.taskTemplate){
+        module.taskTemplate = handlebars.compile($(`#${templateId}`).text());
+    }
+    return module.taskTemplate(data);
+}
 
 // ------------------
 //  end of IIFE
--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -5,28 +5,12 @@ var app = app || {};
 // start of IIFE 
 (function (module){
 
-//TODO create show only property "isProduction"
-module.isProduction= /^(?!localhost\127)/.test(window.location.hostname);
+    // DEFINE VARIABLE BOOKVIEW
+    const BookView = {}; 
 
-// TODO creat "show only method to reveal containers"
-module.showOnly =  (selector) => {
-    $('.container').hide();
-    $(selector).show();
-};
 
-//TODO create render only method  - needs to use module 
-module.render = (templateId, data)  =>  {
-    if(!module.taskTemplate){
-        module.taskTemplate = handlebars.compile($(`#${templateId}`).text());
-    }
-    return module.taskTemplate(data);
-}
 
-//TODO
 
-//TODO
-
-//TODO
 
 // ------------------
 //  end of IIFE
