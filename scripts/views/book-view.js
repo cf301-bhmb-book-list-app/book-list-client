9aa82cbc724fae0bc1f33586591d97b3e754ae3d
matthew <mthwbrwn@gmail.com>
2018-07-25 11:32:15 -0700

    added initIndexpg

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -5,8 +5,14 @@ var app = app || {};
 // start of IIFE 
 (function (module){
 
-    // DEFINE VARIABLE BOOKVIEW
+    // todo DEFINE VARIABLE BOOKVIEW
     const BookView = {}; 
+    // TODO method on BookView initIndexPage- hide container- shows class "book-view" maps book-all
+    // append to #book-list
+    BookView.initIndexPage function () {
+        app.showOnly('book-view');
+        module.Book.all.map(book => $(`#book-list`)append(book.toHtml()));
+    }
 
 
 
